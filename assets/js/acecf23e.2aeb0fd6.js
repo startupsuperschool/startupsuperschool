0d6f32ff61a39c40069e70354caf381260dca964
adseeker <hiadseeker@gmail.com>
2025-05-03 15:44:33 +0200

    Deploy website - based on b4543bf1e2b80c16e33e66bb45beacf11539011f

--- assets/js/acecf23e.2aeb0fd6.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkstartup_super_school=self.webpackChunkstartup_super_school||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
--- assets/js/c15d9823.ce80d245.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkstartup_super_school=self.webpackChunkstartup_super_school||[]).push([[8146],{9328:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
