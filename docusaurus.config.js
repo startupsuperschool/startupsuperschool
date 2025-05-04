e0a2ba000174b190e42c37f2bb36823cd640daad
adseeker <hiadseeker@gmail.com>
2025-05-04 20:38:59 +0200

    Nuovo test articolo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://www.startupsuperschool.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/startupsuperschool/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
